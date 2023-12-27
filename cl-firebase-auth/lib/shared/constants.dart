526add76281df175d803565444faa0993ee63988
safadtm <safadtm2004@gmail.com>
2023-12-27 18:22:45 +0530

    cl-firebase-auth

---
+++ cl-firebase-auth/lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:104684165785:web:cd42ff3217c5b892dc7608";
+  static String apiKey = "AIzaSyCoGIgqcv1JIeInezP49-D0Vhnd54k_n24";
+  static String messagingSenderId = "104684165785";
+  static String projectId = "e-blah";
+}
